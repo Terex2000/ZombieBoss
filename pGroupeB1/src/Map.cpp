212aedb80720204897ef4acb95424c4bf4c233c8
SanoO0 <mathiasmanzano37@gmail.com>
2024-12-03 15:48:51 +0100

    Definition of a map

--- pGroupeB1/src/Map.cpp
+++ pGroupeB1/src/Map.cpp
@@ -1,11 +1,15 @@
 #include "Map.h"
 
-Map::Map()
-{
-    //ctor
+Map::Map(const std::vector<std::vector<int>>& mapData) {
+    for (const auto& row : mapData) {
+        std::vector<Tiles> tileRow;
+        for (int tile : row) {
+            tileRow.emplace_back(tile);
+        }
+        data.push_back(tileRow);
+    }
 }
 
-Map::~Map()
-{
-    //dtor
+const std::vector<std::vector<Tiles>>& Map::getData() const {
+    return data;
 }
