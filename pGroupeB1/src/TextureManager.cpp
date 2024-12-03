5fbfa42328a0135835d654d71b150c39bb4cdd3c
SanoO0 <mathiasmanzano37@gmail.com>
2024-12-03 16:43:11 +0100

    Load textures file with filename

--- pGroupeB1/src/TextureManager.cpp
+++ pGroupeB1/src/TextureManager.cpp
@@ -1,11 +1,25 @@
 #include "TextureManager.h"
+#include <iostream>
 
-TextureManager::TextureManager()
-{
+TextureManager::TextureManager() {
     //ctor
 }
 
-TextureManager::~TextureManager()
-{
+TextureManager::~TextureManager() {
     //dtor
+}
+
+bool TextureManager::loadTexture(const std::string& name, const std::string& filename) {
+    sf::Texture texture;
+    if (!texture.loadFromFile(filename)) {
+        std::cerr << "Error: Failed to load texture from file " << filename << std::endl;
+        return false;
+    }
+    textures[name] = texture;
+    std::cout << "Loaded texture: " << name << " from " << filename << std::endl;
+    return true;
+}
+
+sf::Texture& TextureManager::getTexture(const std::string& name) {
+    return textures.at(name);
 }
