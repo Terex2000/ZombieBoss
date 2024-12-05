886f5e69ca12b6bd8b062dde5be0f180f2ab83ff
SanoO0 <mathiasmanzano37@gmail.com>
2024-12-05 15:30:11 +0100

    Added a method to read tiles with collisions

--- pGroupeB1/include/FileReader.h
+++ pGroupeB1/include/FileReader.h
@@ -1,12 +1,15 @@
+// FileReader.h
 #ifndef FILEREADER_H
 #define FILEREADER_H
 
 #include <vector>
 #include <string>
+#include <unordered_set>
 
 class FileReader {
 public:
     std::vector<std::vector<int>> readMap(const std::string& filename);
+    std::unordered_set<int> readCollisionTypes(const std::string& filename);
 };
 
 #endif // FILEREADER_H
