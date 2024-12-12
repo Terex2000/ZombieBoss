1a1a9a4419c7d57eabf3119615fe2dac3dc9dfd5
SanoO0 <mathiasmanzano37@gmail.com>
2024-12-12 23:24:50 +0100

    Add key pressed to shoot

--- pGroupeB1/src/InputManager.cpp
+++ pGroupeB1/src/InputManager.cpp
@@ -1,4 +1,3 @@
-// InputManager.cpp
 #include "InputManager.h"
 #include <SFML/Window/Keyboard.hpp>
 
@@ -9,11 +8,16 @@ InputManager::~InputManager() {}
 void InputManager::handleInput(PlayerController& playerController) {
     if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left)) {
         playerController.move(-moveSpeed, 0);
+        playerController.getPlayer().setDirection(-1.0f); // Mettre à jour la direction
     }
     if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right)) {
         playerController.move(moveSpeed, 0);
+        playerController.getPlayer().setDirection(1.0f); // Mettre à jour la direction
     }
     if (sf::Keyboard::isKeyPressed(sf::Keyboard::Space)) {
         playerController.jump();
+    }
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Z)) { // Exemple de touche pour tirer
+        playerController.shoot();
     }
 }
