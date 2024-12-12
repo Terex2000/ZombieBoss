c4e81497df8ef6e32c9e083feaabb0028022cbf0
SanoO0 <mathiasmanzano37@gmail.com>
2024-12-12 23:24:21 +0100

    Changing collision method to return a boolean

--- pGroupeB1/include/CollisionManager.h
+++ pGroupeB1/include/CollisionManager.h
@@ -1,20 +1,20 @@
-// CollisionManager.h
 #ifndef COLLISIONMANAGER_H
 #define COLLISIONMANAGER_H
 
 #include "PlayerController.h"
 #include "Map.h"
-#include <vector>
 #include <unordered_set>
 
 class CollisionManager {
 public:
     CollisionManager();
     ~CollisionManager();
-    void checkCollisions(PlayerController& playerController, const Map& map, const std::unordered_set<int>& collisionTypes);
+    bool isColliding(const sf::CircleShape& playerShape, const sf::RectangleShape& tileShape);
+    bool checkPlayerCollisions(const PlayerController& playerController, const Map& map, const std::unordered_set<int>& collisionTypes);
+    bool checkProjectileCollisions(const Projectile& projectile, const Map& map, const std::unordered_set<int>& collisionTypes);
 
 private:
-    bool isColliding(const sf::CircleShape& playerShape, const sf::RectangleShape& tileShape);
+    bool isCollidingWithTile(const sf::CircleShape& shape, const Map& map, const std::unordered_set<int>& collisionTypes);
 };
 
 #endif // COLLISIONMANAGER_H
