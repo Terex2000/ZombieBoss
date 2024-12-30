019b227bfe79e429eb4da561dea3a0534884f4e5
SanoO0 <mathiasmanzano37@gmail.com>
2024-12-30 18:34:18 +0100

    Create basic view of zombie

--- pGroupeB1/src/ZombieView.cpp
+++ pGroupeB1/src/ZombieView.cpp
@@ -1,11 +1,13 @@
 #include "ZombieView.h"
 
-ZombieView::ZombieView()
-{
-    //ctor
+ZombieView::ZombieView() {
+    shape.setRadius(15.0f); // Assuming enemy radius is 15.0f
+    shape.setFillColor(sf::Color::Blue); // Blue color for enemies
 }
 
-ZombieView::~ZombieView()
-{
-    //dtor
+ZombieView::~ZombieView() {}
+
+void ZombieView::draw(sf::RenderWindow& window, const Enemy& enemy) {
+    shape.setPosition(enemy.getPosition());
+    window.draw(shape);
 }
