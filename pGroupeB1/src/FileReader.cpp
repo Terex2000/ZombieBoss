d15f39b826badc129efa3150e04a12377223ad8e
SanoO0 <mathiasmanzano37@gmail.com>
2025-01-02 18:13:56 +0100

    Read teleportation tiles

--- pGroupeB1/src/FileReader.cpp
+++ pGroupeB1/src/FileReader.cpp
@@ -1,4 +1,3 @@
-// FileReader.cpp
 #include "FileReader.h"
 #include <fstream>
 #include <sstream>
@@ -10,7 +9,7 @@ std::vector<std::vector<int>> FileReader::readMap(const std::string& filename) {
     std::string line;
 
     while (std::getline(file, line)) {
-        if (line.find("#COLLISION") == std::string::npos) {
+        if (line.find("#COLLISION") == std::string::npos && line.find("#TELEPORT") == std::string::npos) {
             std::vector<int> row;
             std::istringstream iss(line);
             int tile;
@@ -41,4 +40,24 @@ std::unordered_set<int> FileReader::readCollisionTypes(const std::string& filena
         }
     }
     return collisionTypes;
+}
+
+std::unordered_set<int> FileReader::readTeleportTiles(const std::string& filename) {
+    std::unordered_set<int> teleportTiles;
+    std::ifstream file(filename);
+    std::string line;
+
+    while (std::getline(file, line)) {
+        if (line.find("#TELEPORT") != std::string::npos) {
+            std::istringstream iss(line);
+            std::string temp;
+            int type;
+            iss >> temp; // Skip the "#TELEPORT" part
+            while (iss >> type) {
+                teleportTiles.insert(type);
+                std::cout << "Teleport tile detected: " << type << std::endl; // Debug message
+            }
+        }
+    }
+    return teleportTiles;
 }
