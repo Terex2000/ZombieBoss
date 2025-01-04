a9d09b1c702f0ec02c9aea61d5e89d31ef495d3d
SanoO0 <mathiasmanzano37@gmail.com>
2025-01-05 00:34:50 +0100

    display message when player has 0 lives and 0 health

--- pGroupeB1/src/PlayerController.cpp
+++ pGroupeB1/src/PlayerController.cpp
@@ -9,7 +9,7 @@ PlayerController::PlayerController(float startX, float startY, TextureManager& t
     if (!textureManager.loadTexture("bullet", "assets/img/bullet.png")) {
         std::cerr << "Error: Failed to load bullet texture" << std::endl;
     }
-        if (!textureManager.loadTexture("Idle", "assets/player/idle.png")) {
+    if (!textureManager.loadTexture("Idle", "assets/player/idle.png")) {
         std::cerr << "Error: Failed to load Idle texture" << std::endl;
     }
     if (!textureManager.loadTexture("Jump", "assets/player/jump.png")) {
@@ -18,22 +18,20 @@ PlayerController::PlayerController(float startX, float startY, TextureManager& t
     if (!textureManager.loadTexture("Run", "assets/player/run.png")) {
         std::cerr << "Error: Failed to load Run texture" << std::endl;
     }
-
-            if (!textureManager.loadTexture("Shot_2", "assets/player/Shot_2.png")) {
+    if (!textureManager.loadTexture("Shot_2", "assets/player/Shot_2.png")) {
         std::cerr << "Error: Failed to load Shot_2 texture" << std::endl;
     }
-    
-        if (!textureManager.loadTexture("Idle_Left", "assets/player/idle_left.png")) {
-        std::cerr << "Error: Failed to load Shot_2 texture" << std::endl;
+    if (!textureManager.loadTexture("Idle_Left", "assets/player/idle_left.png")) {
+        std::cerr << "Error: Failed to load Idle_Left texture" << std::endl;
     }
-            if (!textureManager.loadTexture("Jump_Left", "assets/player/jump_left.png")) {
-        std::cerr << "Error: Failed to load Shot_2 texture" << std::endl;
+    if (!textureManager.loadTexture("Jump_Left", "assets/player/jump_left.png")) {
+        std::cerr << "Error: Failed to load Jump_Left texture" << std::endl;
     }
-        if (!textureManager.loadTexture("Run_Left", "assets/player/run_left.png")) {
-        std::cerr << "Error: Failed to load Run texture" << std::endl;
+    if (!textureManager.loadTexture("Run_Left", "assets/player/run_left.png")) {
+        std::cerr << "Error: Failed to load Run_Left texture" << std::endl;
     }
     if (!textureManager.loadTexture("Shot_2_Left", "assets/player/Shot_2_left.png")) {
-        std::cerr << "Error: Failed to load Shot_2 texture" << std::endl;
+        std::cerr << "Error: Failed to load Shot_2_Left texture" << std::endl;
     }
 }
 
@@ -80,6 +78,14 @@ void PlayerController::update(float deltaTime, const sf::View& cameraView) {
     }
     handleState();
     projectileController.update(deltaTime, cameraView);
+
+     std::cout << "Lives" << player.getLives() << std::endl;
+    std::cout << "health" << player.getHealth() << std::endl;
+    // Check if the player has no lives left
+    if (player.getLives() <= 0) {
+        std::cout << "Game Over! The player has no lives left." << std::endl;
+        // Handle game over logic here (e.g., transition to a game over screen)
+    }
 }
 
 // Sets the player's position.
